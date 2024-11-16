2ac63784b1f2b62daacb961fa91fb4b006e5c124
Devesh Gupta <138644550+deveshusg@users.noreply.github.com>
2024-11-16 16:15:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
 st.title('Streamlit Intro')
-
+st.header('Header')
+st.subheader('Subheader')
+st.text('Text')
 st.write('Streamlit is s python framework for web applicstions.')
