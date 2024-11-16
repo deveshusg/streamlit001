ce90ad170454a2acdbfdd43aed946d37acb88bde
Devesh Gupta <138644550+deveshusg@users.noreply.github.com>
2024-11-16 16:26:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,10 @@ st.subheader('Subheader')
 st.text('Text')
 st.write('Streamlit is s python framework for web applicstions.')
 st.markdown('**Hello** *World*')
+st.write('
+Element	Markdown Syntax
+Heading	# H1
+        ## H2
+        ### H3
+Bold	**bold text**
+Italic	*italicized text* ')
