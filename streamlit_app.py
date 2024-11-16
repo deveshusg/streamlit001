16f4cdf6197ec37481dc76ed7458daa168cb095d
Devesh Gupta <138644550+deveshusg@users.noreply.github.com>
2024-11-16 16:54:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,9 @@ st.markdown('[Google](https://www.google.com)')
 #st.markdown('![alt text](image.jpg)')
 st.caption('caption')
 st.latex(r'\begin{pmatrix}a&b\\c&d\end{pmatrix}')
-code=(""
+code=("""
         import numpy as np
         array=np.array(1,2,3,4,5,6,7,8,9,10).reshape(5,2)
         new_array=array*2
-      "")
+      """)
 st.code(code)
